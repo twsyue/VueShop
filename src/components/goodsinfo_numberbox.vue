81b8b4fc7d263a322cd52e5047aa484a7d7e09e7
TWSyue <twsyue@163.com>
2019-09-18 14:53:50 +0800

    添加组件

---
+++ src/components/goodsinfo_numberbox.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="mui-numbox" data-numbox-min='1' >
+        <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+        <input id="test" class="mui-input-numbox" type="number" value="1"
+               @change="CountChange" ref="numbox" />
+        <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+    </div>
+</template>
+
+<script>
+    import mui from '../assets/lib/mui/js/mui.js';
+    export default {
+        mounted(){//引入的是在mian.js所以加上this
+            mui('.mui-numbox').numbox();
+        },
+        methods: {
+            CountChange(){//文本框数据被修改的时候
+                this.$emit('Getcount',parseInt(this.$refs.numbox.value));
+            }
+        },
+        props: [
+            "max"
+        ],
+        watch: { //属性监听 监听 max
+            max: function(newVal){
+                mui(".mui-numbox").numbox().setOption("max",newVal)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
